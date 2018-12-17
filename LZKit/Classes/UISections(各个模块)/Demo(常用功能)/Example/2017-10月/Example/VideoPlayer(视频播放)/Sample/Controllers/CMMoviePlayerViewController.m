5001a17eef140d1684ad8aac9722719cdd394195
宁小陌y <136769890@qq.com>
2018-12-17 12:51:13 +0800

    修改

--- LZKit/Classes/UISections(各个模块)/Demo(常用功能)/Example/2017-10月/Example/VideoPlayer(视频播放)/Sample/Controllers/CMMoviePlayerViewController.m
+++ LZKit/Classes/UISections(各个模块)/Demo(常用功能)/Example/2017-10月/Example/VideoPlayer(视频播放)/Sample/Controllers/CMMoviePlayerViewController.m
@@ -144,7 +144,7 @@
 
 - (IBAction)playNewVideo:(UIButton *)sender {
     self.playerModel.title            = @"这是新播放的视频";
-    self.playerModel.videoURL         = [NSURL URLWithString:@"http://baobab.wdjcdn.com/1456665467509qingshu.mp4"];
+    self.playerModel.videoURL         = [NSURL URLWithString:@"http://pandavideo.sctv.com:41086/201710/27/1d64ec292b992000_MOBILE_HD_720X405_800_CBR.mp4"];
     // 设置网络封面图
     self.playerModel.placeholderImageURLString = @"http://img.wdjimg.com/image/video/447f973848167ee5e44b67c8d4df9839_0_0.jpeg";
     // 从xx秒开始播放视频
